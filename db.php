78c057d6c4cf86f5325f0481580f1058f1864a88
Fredrex Salac <fredrexsalac@gmail.com>
2025-11-23 01:43:50 +0800

    Roastrack barbecue First commit by Dominica Napoles

--- db.php
+++ db.php
@@ -6,13 +6,14 @@ function db(): PDO {
   static $pdo = null;
   if ($pdo instanceof PDO) return $pdo;
 
-  $host = '127.0.0.1';
-  $db   = 'roastrack';
-  $user = 'root';
-  $pass = '';
+  $host = getenv('DB_HOST') ?: 'db.fr-pari1.bengt.wasmernet.com';
+  $port = getenv('DB_PORT') ?: '10272';
+  $db   = getenv('DB_NAME') ?: 'dbhQeyWKy3w75MzrX86oRDec';
+  $user = getenv('DB_USER') ?: '1f503aaa78138000849f189a3aea';
+  $pass = getenv('DB_PASS') ?: '06921f50-3aaa-7958-8000-9e75d554b5bb';
   $charset = 'utf8mb4';
 
-  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  $dsn = "mysql:host={$host};port={$port};dbname={$db};charset={$charset}";
   $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
